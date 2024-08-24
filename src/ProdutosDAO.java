2a33881ec4e37df4b3e64cbc83468171d9321970
GuilhermeEstevan <guilhermeestevangui@hotmail.com>
2024-08-24 15:03:41 -0300

    Implementação da função listarProdutosVendidos() em ProdutosDAO.

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -124,6 +124,46 @@ public class ProdutosDAO {
     }
 
     
+    
+    public ArrayList<ProdutosDTO> listarProdutosVendidos() {
+        conn = new ConectaDAO().connectDB();
+        ArrayList<ProdutosDTO> listaVendidos = new ArrayList<>();
+        String sql = "SELECT * FROM produtos WHERE status = 'Vendido'";
+
+        try {
+            prep = conn.prepareStatement(sql);
+            resultset = prep.executeQuery();
+
+            while (resultset.next()) {
+                ProdutosDTO produto = new ProdutosDTO();
+                produto.setId(resultset.getInt("id"));
+                produto.setNome(resultset.getString("nome"));
+                produto.setValor(resultset.getInt("valor"));
+                produto.setStatus(resultset.getString("status"));
+
+                listaVendidos.add(produto);
+            }
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Erro ao listar produtos vendidos: " + erro.getMessage());
+        } finally {
+            try {
+                if (resultset != null) {
+                    resultset.close();
+                }
+                if (prep != null) {
+                    prep.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                JOptionPane.showMessageDialog(null, "Erro ao fechar conexão: " + e.getMessage());
+            }
+        }
+
+        return listaVendidos;
+    }
+
         
 }
 
